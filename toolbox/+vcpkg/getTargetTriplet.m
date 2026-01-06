8325b1a18fa898bc46ff62d52b9aa403d90f521f
Pablo Romero <promero@mathworks.com>
2026-01-05 21:36:12 -0600

    Clean up target triplet logic and make it mainly dependent on installed SLRT.

--- toolbox/+vcpkg/getTargetTriplet.m
+++ toolbox/+vcpkg/getTargetTriplet.m
@@ -2,18 +2,21 @@ function triplet = getTargetTriplet
 
 % Copyright 2025 The MathWorks, Inc.
 
+% Initialize target triplet
 triplet = '';
 
-if isMATLABReleaseOlderThan('R2026a') && ~isMATLABReleaseOlderThan('R2020b')
-    spkgInfo = matlabshared.supportpkg.getInstalled;
-    if ~isempty(spkgInfo) && any(contains({spkgInfo.Name}, 'Simulink Real-Time Target Support Package'))
+% Get simple list of installed toolboxes
+listToolbox = ver;
+listToolbox = {listToolbox.Name}';
+
+% Figure out target triplet based on installed target toolbox
+if any(contains(listToolbox,'Simulink Real-Time'))
+    if isMATLABReleaseOlderThan('R2026a') && ~isMATLABReleaseOlderThan('R2020b')
         triplet = 'x64-qnx-static';
-    end
-elseif isMATLABReleaseOlderThan('R2020b')
-    triplet = 'x86-windows-static';
-else
-    % MATLAB R2026a or later
-    if exist('speedgoat','file')
+    elseif isMATLABReleaseOlderThan('R2020b')
+        triplet = 'x86-windows-static';
+    else
+        % MATLAB R2026a or later
         triplet = 'x64-speedgoat-linux-static';
     end
 end
