993f01b20992e4d8b56b4edc1e0eb341e7c9ac26
Pablo Romero <promero@mathworks.com>
2026-01-06 12:10:42 -0600

    Minor fixes and improvements.

--- toolbox/overlay-triplets/speedgoat-linux-toolchain.cmake
+++ toolbox/overlay-triplets/speedgoat-linux-toolchain.cmake
@@ -4,17 +4,31 @@
 cmake_minimum_required(VERSION 3.20)
 
 # Tell CMake we target Linux
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Linux CACHE STRING "")
 
-# Allow caller to choose architecture; default to x86_64
-set(CMAKE_SYSTEM_PROCESSOR "x86_64" CACHE STRING "Speedgoat Linux target arch (aarch64|x86_64)")
+# Set target processor
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+     set(CMAKE_SYSTEM_PROCESSOR x86_64 CACHE STRING "")
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+     set(CMAKE_SYSTEM_PROCESSOR i686 CACHE STRING "")
+     string(APPEND VCPKG_C_FLAGS " -m32")
+     string(APPEND VCPKG_CXX_FLAGS " -m32")
+     string(APPEND VCPKG_LINKER_FLAGS " -m32")
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+     set(CMAKE_SYSTEM_PROCESSOR armv7l CACHE STRING "")
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+     set(CMAKE_SYSTEM_PROCESSOR aarch64 CACHE STRING "")
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "riscv64")
+     set(CMAKE_SYSTEM_PROCESSOR riscv64 CACHE STRING "")
+endif()
 
 # Compilers
 set(CMAKE_C_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-speedgoat-linux-gcc)
 set(CMAKE_CXX_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-speedgoat-linux-g++)
 
 # Sysroot
-set(CMAKE_SYSROOT "$ENV{SDKTARGETSYSROOT}")
+string(REPLACE "\"" "" SDKTARGETSYSROOT "$ENV{SDKTARGETSYSROOT}")
+set(CMAKE_SYSROOT "${SDKTARGETSYSROOT}")
 
 # Optimize for target architecture
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
